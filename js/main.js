e9440d2bef41b2b06cd89a1fa2a455e15c4ea330
erikzaadi <erik.zaadi@gmail.com>
2015-05-09 23:33:06 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+	//SMOOTH SCROLLING
+  $('a[href*=#]:not([href=#])').click(function() {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    if (target.length) {
+      $('html,body').stop(true, true).animate({ scrollTop: target.offset().top}, 500);
+    }
+    return false;
+  });
+
+	//Open links in new window
+	$("a[rel=external]").each(function(){
+		this.target = "_blank";
+	});
+
+	//default validate action
+	$('form').attr('novalidate',true);
+
+	$("a.print").click(function(){
+		window.print();
+		return false;
+	});
+});
+
+
